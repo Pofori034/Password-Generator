74ec6ae4f818dcbe7c3f59a03a60bd18d77dc3b0
Frahnk01 <37987576+Frahnk01@users.noreply.github.com>
2021-01-13 16:31:12 -0800

    Model GUI developed
    GUI files for password generator.

---
+++ callpasswordgen.py
@@ -0,0 +1,26 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import pyperclip
+import passwordgen
+
+class MyApp(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.ui = passwordgen.Ui_mainWindow()
+		self.ui.setupUi(self)
+		self.ui.generateButton.clicked.connect(self.generate)
+		self.ui.copyButton.clicked.connect(self.copy)
+
+		self.show()
+	def generate(self):
+		self.passwordlength = self.ui.passLine.text()
+
+	def copy(self):
+		self.password = self.ui.password.text()
+		pyperclip.copy(self.password)
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	w = MyApp()
+	w.show()
+	sys.exit(app.exec_())
